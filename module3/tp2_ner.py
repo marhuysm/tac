4bd896990960965019ed6ae44777921e83b31fb0
marhuysm <huysmanm@gmail.com>
2020-11-02 17:50:21 +0100

    tp2 étape 6

--- module3/tp2_ner.py
+++ module3/tp2_ner.py
@@ -8,32 +8,38 @@ from spacy.lang.fr.examples import sentences
 
 nlp = spacy.load('fr_core_news_sm')
 
+n = 100000 
+
+# Rem : pour n, 100000 ne fonctionnait pas : le programme était directement tué
+
 def search():
-    text = open("1929.txt", encoding='utf-8').read()[:100000]
+    text = open("1929.txt", encoding='utf-8').read()[:n]
     doc = nlp(text)
-    people = defaultdict(int)
     location = defaultdict(int)
+    people = defaultdict(int)
     organisation = defaultdict(int)
     for ent in doc.ents:
-        if ent.label_ == "PER" and len(ent.text) > 3:
-            people[ent.text] += 1
-        elif ent.label == "LOC" and len(ent.text) > 3:
+        if ent.label_ == "LOC" and len(ent.text) > 3:
             location[ent.text] += 1
+        elif ent.label_ == "PER" and len(ent.text) > 3:
+            people[ent.text] += 1
         elif ent.label == "ORG" and len(ent.text) > 3:
             organisation[ent.text] += 1   
         else :  
             continue 
-    sorted_people = sorted(people.items(), key=lambda kv: kv[1], reverse=True)
     sorted_location = sorted(location.items(), key=lambda kv: kv[1], reverse=True)
+    sorted_people = sorted(people.items(), key=lambda kv: kv[1], reverse=True)
     sorted_organisation = sorted(organisation.items(), key=lambda kv : kv[1], reverse=True)
-    for person, freq in sorted_people[:20]:
-        print(f"{person}, a person, appears {freq} times in the corpus")
-    for loc, freq in sorted_location[:20]:
-        print(f"{loc}, a location, appears {freq} times in the corpus")
     for org, freq in sorted_organisation[:20]:
         print(f"{org}, an organisation, appears {freq} times in the corpus")
+    for loc, freq in sorted_location[:20]:
+        print(f"{loc}, a location, appears {freq} times in the corpus")
+    for person, freq in sorted_people[:20]:
+        print(f"{person}, a person, appears {freq} times in the corpus")
 
-# Il ne semble pas y avoir d'entités reconnues comme des organisations ou des lieux dans le texte 1929.txt
+# PB : LOC ne semble pas avoir été détecté.. Pourtant, en utilisant le code qui suit, des entitées taggées comme "LOC" sont bien présentes : 
+# for ent in doc.ents:
+#   print(ent.label_, ' | ', ent.text)
 
 if __name__ == "__main__":
     try:

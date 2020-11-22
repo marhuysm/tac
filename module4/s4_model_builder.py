22cd977654e48c9c22beb8f9cf6e47130e1718d1
marhuysm <huysmanm@gmail.com>
2020-11-22 11:41:06 +0100

    fin tp3

--- module4/s4_model_builder.py
+++ module4/s4_model_builder.py
@@ -2,6 +2,7 @@
 
 import logging
 import sys
+import os
 
 from gensim.models.phrases import Phrases, Phraser
 from gensim.models import Word2Vec
@@ -9,7 +10,7 @@ from gensim.models import Word2Vec
 import nltk
 from nltk.tokenize import wordpunct_tokenize
 
-nltk.data.path.append("/home/max/nltk_data")
+nltk.data.path.append("/home/marie/nltk_data")
 
 logging.basicConfig(format='%(asctime)s : %(threadName)s : %(levelname)s : %(message)s', level=logging.INFO)
 
@@ -22,12 +23,12 @@ class MySentences(object):
         for line in open(self.filename, encoding='utf-8', errors="backslashreplace"):
             yield [w.lower() for w in wordpunct_tokenize(line)]
 
-infile = f"data/sents.txt"
+infile = f"../data/sents.txt"
 sentences = MySentences(infile)
 phrases = Phrases(sentences)
 bigram = Phraser(phrases)
 trigram = Phrases(bigram[sentences])
-corpus = list(trigram[bigram[sentences]])
-model = Word2Vec(corpus, size=32, window=5, min_count=5, workers=4, iter=5)
-outfile = f"data/bulletins.model"
+corpus = trigram[bigram[sentences]]
+model = Word2Vec(corpus, size=32, window=5, min_count=5, workers=4, iter = 5)
+outfile = f"../data/bulletins.model"
 model.save(outfile)
